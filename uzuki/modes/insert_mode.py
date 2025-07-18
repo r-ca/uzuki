27bd84fdb989937916cca8b2b6b307ca1df8fe33
rca <_@r0m.me>
2025-07-18 14:51:38 +0900

    add spacekey handler, arrow-keys handler

--- uzuki/modes/insert_mode.py
+++ uzuki/modes/insert_mode.py
@@ -14,6 +14,25 @@ class InsertMode(BaseMode):
             if cur.col > 0:
                 buf.delete(cur.row, cur.col-1)
                 cur.move(0, -1, buf)
+        elif key == key.SPACE:
+            if cur.col < len(buf.lines[cur.row]):
+                buf.insert(cur.row, cur.col, ' ')
+                cur.move(0, 1, buf)
+        # Cursor navigation keys
+        elif key == Key.LEFT:
+            if cur.col > 0:
+                cur.move(0, -1, buf)
+        elif key == Key.RIGHT:
+            if cur.col < len(buf.lines[cur.row]):
+                cur.move(0, 1, buf)
+        elif key == Key.UP:
+            if cur.row > 0:
+                cur.move(-1, 0, buf)
+        elif key == Key.DOWN:
+            if cur.row < len(buf.lines) - 1:
+                cur.move(1, 0, buf)
+
+        # Handle printable characters
         else:
             buf.insert(cur.row, cur.col, chr(key.value))
             cur.move(0, 1, buf)
